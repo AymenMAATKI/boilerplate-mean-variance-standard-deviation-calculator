b7352c2cddce221d9ec1284851be62d84b5a1bdd
AymenMAATKI <68022315+AymenMAATKI@users.noreply.github.com>
2023-09-18 18:15:25 +0000

    commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,37 @@
 import numpy as np
 
 def calculate(list):
+  
+  #test if the list is shorter than 9 flag error
+  if len(list) < 9: 
+    raise ValueError("List must contain nine numbers.")
+
+  #create the numpy array and the 2d numpy array from list
+  np_array_flattened=np.array(list)
+  np_array=np.array([list[:3],list[3:6],list[6 :]])
+  
+
+  #make the calculation dict
+  calculations={
+  'mean': [np.mean(np_array, axis=0).tolist(),
+           np.mean(np_array, axis=1).tolist(),     
+           np.mean(np_array_flattened)],
+  'variance': [np.var(np_array, axis=0).tolist(),
+               np.var(np_array, axis=1).tolist(),     
+           np.var(np_array_flattened)],
+  'standard deviation': [np.std(np_array, axis=0).tolist(),
+                         np.std(np_array, axis=1).tolist(),
+                         np.std(np_array_flattened)],
+  'max': [np.max(np_array, axis=0).tolist(),
+          np.max(np_array, axis=1).tolist(),     
+          np.max(np_array_flattened)],
+  'min': [np.min(np_array, axis=0).tolist(),
+          np.min(np_array, axis=1).tolist(),     
+          np.min(np_array_flattened)],
+  'sum': [np.sum(np_array, axis=0).tolist(),
+          np.sum(np_array, axis=1).tolist(),     
+          np.sum(np_array_flattened)],
+  }
 
 
-
-
-    return calculations
+  return calculations
